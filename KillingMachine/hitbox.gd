5e4819b300ea56ca14c0426c6f37877a4e4e1508
CaioCintra <caiocintra2000@gmail.com>
2023-10-24 20:35:23 -0300

    Adição de artes de dano e do inimigo Painkiller

--- KillingMachine/hitbox.gd
+++ KillingMachine/hitbox.gd
@@ -3,5 +3,8 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("players"):
 		body.velocity.y = body.JUMP_VELOCITY
-		print(body.name)
+		body.isHurt = true
+		
+
+		
  
--- KillingMachine/levels/scripts/Vic.gd
+++ KillingMachine/levels/scripts/Vic.gd
@@ -7,8 +7,10 @@ const JUMP_VELOCITY = -430.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var isHurt := false
 
 @onready var animation := $anim as AnimatedSprite2D
+@onready var remote_transform := $remote as RemoteTransform2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,6 +28,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
+	
 	if direction != 0:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
@@ -39,5 +42,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
+		
+	if isHurt:
+		animation.play("hurt")
 
 	move_and_slide()
