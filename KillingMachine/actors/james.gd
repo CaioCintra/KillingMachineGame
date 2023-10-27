5af2a4fd0728c3353c17afd341ef1ad0e7f7939a
CaioCintra <caiocintra2000@gmail.com>
2023-10-27 09:55:37 -0300

    Poder James

--- KillingMachine/actors/james.gd
+++ KillingMachine/actors/james.gd
@@ -5,6 +5,7 @@ const JUMP_VELOCITY = -430.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var _double_jump: bool = false
 var isHurt := false
 var ativo = false
 
@@ -12,20 +13,23 @@ var ativo = false
 @onready var remote_transform := $remote as RemoteTransform2D
 
 func _physics_process(delta):
-
 	# Add the gravity.
 	velocity.y += gravity * delta
 	
 	if is_on_floor():
 		is_jumping = false
+		_double_jump = false
 	
 	if ativo:
 		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or not _double_jump):
 			velocity.y = JUMP_VELOCITY
 			$sound_jump.play()
 			is_jumping = true
-	
+			if not is_on_floor():
+				velocity.y = JUMP_VELOCITY - 100
+				_double_jump = true
+				
 		# Get the input direction and handle the movement/deceleration.
 		var direction = round(Input.get_axis("ui_left", "ui_right"))
 		
