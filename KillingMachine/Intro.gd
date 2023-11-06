ece4303154169f15efa8008dc2e1bd1d2e8ce7eb
CaioCintra <caiocintra2000@gmail.com>
2023-11-06 11:06:02 -0300

    Melhorias primeira release

--- KillingMachine/Intro.gd
+++ KillingMachine/Intro.gd
@@ -1,24 +1,23 @@
 extends Node2D
 
+@onready var start = $CanvasLayer/VBoxContainer/Start
 
-
-
+func _ready():
+	start.grab_focus()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://levels/world_01.tscn")
 
 
 func _on_controls_pressed():
-	pass # Replace with function body.
-
-
+	pass # Replace with function body
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 
 func _on_start_touch_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://levels/world_01.tscn")
 
 
 func _on_controls_touch_pressed():
